d3272c97b5b6648b9aa5ea94bd4d7f4bdc83d2e6
Pei1133 <pei.7969pei.7969@gmail.com>
2017-12-14 20:47:19 +0800

    first commit

---
+++ Painting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Painting
+//
+//  Created by 黃珮鈞 on 2017/12/14.
+//  Copyright © 2017年 黃珮鈞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
