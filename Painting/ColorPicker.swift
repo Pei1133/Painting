e478493c7c92e691894adac8d93dee250a997c84
Pei1133 <pei.7969pei.7969@gmail.com>
2017-12-17 16:33:02 +0800

    try scrollView

--- Painting/ColorPicker.swift
+++ Painting/ColorPicker.swift
@@ -77,10 +77,3 @@ class ColorPicker: UIView {
         self.setNeedsDisplay()
     }
 }
-/*
- // Only override draw() if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- override func draw(_ rect: CGRect) {
- // Drawing code
- }
- */
