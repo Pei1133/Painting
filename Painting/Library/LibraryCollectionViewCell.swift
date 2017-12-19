ea416905f2ef14bef81e1b34d79c856b6fc1a785
Pei1133 <pei.7969pei.7969@gmail.com>
2017-12-19 17:35:29 +0800

    set LibraryCollectionViewCell model

---
+++ Painting/Library/LibraryCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  LibraryCollectionViewCell.swift
+//  Painting
+//
+//  Created by 黃珮鈞 on 2017/12/19.
+//  Copyright © 2017年 黃珮鈞. All rights reserved.
+//
+
+import UIKit
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+
+    // MARK: Property
+
+    @IBOutlet private(set) weak var previewImageView: UIImageView!
+
+    // MARK: Life Cycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        setUpPreviewImageView()
+
+    }
+
+    // MARK: Set Up
+
+    private func setUpPreviewImageView() {
+
+        let imageView = previewImageView!
+
+        imageView.clipsToBounds = true
+
+        imageView.contentMode = .scaleAspectFill
+
+        imageView.layer.borderColor = UIColor(
+            red: 0.0 / 255.0,
+            green: 0.0 / 255.0,
+            blue: 0.0 / 255.0,
+            alpha: 1.0
+            ).cgColor
+
+        imageView.layer.borderWidth = 0.2
+
+//        imageView.layer.cornerRadius = 1.0
+    }
+
+}
--- Painting/Library/LibraryCollectionViewController.swift
+++ Painting/Library/LibraryCollectionViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class LibraryCollectionViewController: UICollectionViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -43,20 +43,19 @@ class LibraryCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 6
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+
         // Configure the cell
-    
+
         return cell
     }
 
@@ -90,5 +89,5 @@ class LibraryCollectionViewController: UICollectionViewController {
     
     }
     */
-    
+
 }
