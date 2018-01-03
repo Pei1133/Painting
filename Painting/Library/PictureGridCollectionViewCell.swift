dcc97e4ec57cee4af98d1be3c9678f5da6b5a7f1
Pei1133 <pei.7969pei.7969@gmail.com>
2018-01-03 17:44:38 +0800

    add pictureView of collectionView cell xib

--- Painting/Library/PictureGridCollectionViewCell.swift
+++ Painting/Library/PictureGridCollectionViewCell.swift
@@ -14,7 +14,7 @@ class PictureGridCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var pictureImageView: UIImageView!
 
-//    @IBOutlet weak var pictureView: UIView!
+    @IBOutlet weak var pictureView: UIView!
 
     // MARK: Life Cycle
 
@@ -23,7 +23,7 @@ class PictureGridCollectionViewCell: UICollectionViewCell {
 
         setUpPictureImageView()
 
-//        setUpPictureView()
+        setUpPictureView()
 
     }
 
@@ -33,54 +33,47 @@ class PictureGridCollectionViewCell: UICollectionViewCell {
 
         let imageView = pictureImageView!
 
-        imageView.clipsToBounds = true
+//        imageView.clipsToBounds = true
 
         imageView.contentMode = .center
 
-        imageView.layer.cornerRadius = 8.0
+//        imageView.layer.cornerRadius = 8.0
 
-        imageView.layer.borderWidth = 0.2
+//        imageView.layer.borderWidth = 0.2
 
         imageView.backgroundColor = UIColor.white
 
-        imageView.layer.shadowColor = Colors.coolGray.cgColor
+//        imageView.layer.shadowColor = Colors.coolGray.cgColor
 
-        imageView.layer.shadowRadius = 1.0
+//        imageView.layer.shadowRadius = 1.0
 
-        imageView.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+//        imageView.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
 
-        imageView.layer.shadowOpacity = 0.26
+//        imageView.layer.shadowOpacity = 0.26
 
     }
 
-//    private func setUpPictureView() {
-//
-//        let view = pictureView!
-//
-//        view.clipsToBounds = true
-//
-//        view.contentMode = .center
-//
-//        view.layer.cornerRadius = 1.0
-//
-//        view.layer.borderWidth = 0.2
-//
-//        view.backgroundColor = UIColor.blue
-//
-////        view.backgroundColor = UIColor(
-////            red: 255.0 / 255.0,
-////            green: 255.0 / 255.0,
-////            blue: 201.0 / 255.0,
-////            alpha: 1.0
-////        )
-//
-//        view.layer.shadowColor = UIColor.black.cgColor
-//
-//        view.layer.shadowRadius = 1.0
-//
-//        view.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-//
-//        view.layer.shadowOpacity = 0.26
-//
-//    }
+    private func setUpPictureView() {
+
+        let view = pictureView!
+
+        view.clipsToBounds = true
+
+        view.contentMode = .center
+
+        view.layer.cornerRadius = 8.0
+
+        view.layer.borderWidth = 0.2
+
+        view.backgroundColor = Colors.littleBlue
+        
+        view.layer.shadowColor = UIColor.black.cgColor
+
+        view.layer.shadowRadius = 1.0
+
+        view.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+
+        view.layer.shadowOpacity = 0.26
+
+    }
 }
