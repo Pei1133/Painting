98a03866b9d9ea6fa90c52885b0ff71600ac95da
Pei1133 <pei.7969pei.7969@gmail.com>
2018-01-08 03:49:29 +0800

    Crashlytics test crash

--- Painting/Library/PictureGridCollectionViewController.swift
+++ Painting/Library/PictureGridCollectionViewController.swift
@@ -35,18 +35,8 @@ class PictureGridCollectionViewController: UICollectionViewController {
         setUpGradientColor()
         setUpNavigationBar()
         setUpBlurEffect()
-        let button = UIButton(type: .roundedRect)
-        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-        button.setTitle("Crash", for: [])
-        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-        view.addSubview(button)
-    }
-    
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-        Crashlytics.sharedInstance().crash()
     }
 
-    
     func setUpCollectionView() {
 
         self.collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: flowLayout)
